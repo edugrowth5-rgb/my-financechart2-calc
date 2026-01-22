89a1b1d4c288b350e44b53e92e5dc2fad9d5ca44
edugrowth5-rgb <edugrowth5@gmail.com>
2026-01-22 21:26:49 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,19 @@
-const CACHE_NAME = 'citizen-pro-v8-english'; // Version Updated
+const CACHE_NAME = 'citizen-force-v9'; // वर्जन बढ़ाकर v9 कर दिया है
 const assets = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (e) => {
-  self.skipWaiting();
+  self.skipWaiting(); // पुराने सर्विस वर्कर को तुरंत हटा देगा
   e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(assets)));
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then((keys) => {
-    return Promise.all(keys.map(k => {
-      if(k !== CACHE_NAME) return caches.delete(k);
-    }));
-  }));
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map(k => {
+        if(k !== CACHE_NAME) return caches.delete(k);
+      }));
+    }).then(() => self.clients.claim()) // नए वर्कर को तुरंत कंट्रोल देगा
+  );
 });
 
 self.addEventListener('fetch', (e) => {
